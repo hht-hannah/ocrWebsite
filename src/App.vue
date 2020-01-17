f1c6b48d26da27cc89abc8373437318e9127a9a7
Hanting <huanght@hku.hk>
2020-01-17 13:47:48 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,157 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="left-container">
+      <img alt="logo" src="./assets/daikin.png" />
+      <p>智能合同审阅</p>
     </div>
-    <router-view/>
+    <div class="right-container">
+      <div class="upload-container">
+        <el-upload
+          class="upload-demo"
+          drag
+          action="https://jsonplaceholder.typicode.com/posts/"
+          multiple
+        >
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">
+            将文件拖到此处，或
+            <em>点击上传</em>
+          </div>
+        </el-upload>
+      </div>
+      <div class="button-container">
+        <el-upload
+          class="upload-demo"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          multiple
+        >
+          <el-button type="primary" round>上传文件</el-button>
+        </el-upload>
+        <el-button round>清除缓存</el-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  height: 100vh;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  background-image: url("./assets/background.jpg");
+  background-size: 100% 100vh;
 }
 
-#nav {
-  padding: 30px;
+.left-container {
+  float: left;
+  width: 40vw;
+  text-align: center;
 
-  a {
+  img {
+    margin: 40px;
+  }
+  p {
+    margin-top: -60px;
+    font-size: 44px;
     font-weight: bold;
-    color: #2c3e50;
+    color: #4682b4;
+  }
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
+.right-container {
+  float: right;
+  margin: 40px;
+  height: 80vh;
+  width: 50vw;
+  background-color: rgba(255, 255, 255, 0.8);
+  .upload-container {
+    margin: 10vh 5vw;
+
+    .el-upload-dragger {
+      width: 40vw;
+      height: 30vh;
     }
+  }
+
+  .button-container {
+    margin-top: 10vh;
+    text-align: center;
+
+    .upload-demo {
+      width: 240px;
+      display: inline-block;
+    }
+
+    .el-button {
+      margin: 20px;
+      display: inline-block;
+      width: 200px;
+    }
+  }
+}
+
+@media only screen and (max-width: 800px) {
+  #app {
+    height: 100vh;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    text-align: center;
+    background-image: url("./assets/background.jpg");
+    background-size: 100% 100vh;
+  }
+
+  .left-container {
+    width: 100vw;
+    text-align: center;
+    display: block;
+    img {
+      margin: 0;
+      width: 50%;
+    }
+
+    p {
+      margin-top: -80px;
+      font-size: 36px;
+      font-weight: bold;
+      color: #4682b4;
+    }
+  }
+
+  .right-container {
+    margin: 0 10vw;
+    width: 80vw;
+    height: 50vh;
+    text-align: center;
+    display: block;
+
+    .upload-container {
+    margin: 5vh 5vw;
+
+    .el-upload-dragger {
+      width: 40vw;
+      height: 30vh;
+    }
+  }
+
+  .button-container {
+    margin-top: -5vh;
+    text-align: center;
+
+    .upload-demo {
+      width: 240px;
+      display: inline-block;
+    }
+
+    .el-button {
+      margin: 20px;
+      display: inline-block;
+      width: 200px;
+    }
+  }
   }
 }
 </style>
